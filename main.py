f9b55ab9d141743811e78940bc93f64063d4d5ab
Tikaani <dara.stormy@gmail.com>
2025-03-13 15:29:56 +0200

    feat: add method randomly_move

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 class Field:
     def __init__(self, tiger, rabbits):
         self.size = 5
@@ -26,4 +28,31 @@ class Rabbit:
 
     def to_catch(self): # Меняет атрибут класса is_catched на True или False
         self.is_catched = True
+
+class Tiger:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.state = 'hunting'
+        self.lucky_jump = 0.5
+
+    def randomly_move(self):
+
+        self.x = random.choice([-1, 0, 1])
+
+        if self.x < 0:
+            self.x = 0
+        elif self.x > 4:
+            self.x = 0
+
+        self.y = random.choice([-1, 0, 1])
+
+        if self.y < 0:
+            self.y = 0
+        elif self.y > 4:
+            self.y = 0
+
+    def update_state(self):
+        if self.state == 'hunting':
+            print('Тигр охотится')
 
